2c2b866986a88a09e51ac96911cc946fc593aac8
Weilei Wang <weilei.wang@gmail.com>
2017-12-08 22:50:05 -0800

    comment out plot

--- stepper_Th_v2.m
+++ stepper_Th_v2.m
@@ -27,7 +27,7 @@ PFdiv_f = buildPFD_cons_SV(M3d,p,grd);
 PFdiv_f(end,end) = PFdiv_f(end-1,end-1);
 
 t = 0;	
-dt = 1;
+dt = 1/4;
 I = speye(length(U238));
 A = I+(dt/2)*PFdiv_f;
 B = I-(dt/2)*PFdiv_f;
@@ -53,83 +53,83 @@ for ik = 1:nstep
   TH0  = mfactor(FA,(B*[0.001;TH0(2:end)]+dTH0dt*dt));
   t = t+dt;
   
-  if mod(ik,48) == 0
+  %if mod(ik,48) == 0
     
-    set(0,'CurrentFigure',1);
-    subplot(3,2,1)
-    plot(t,d234(24),'rs'); hold on; drawnow
-    ylabel('[^2^3^4Th_d] (dpm/m^3)')
-    xlabel('time, (day)')
+    %set(0,'CurrentFigure',1);
+    %subplot(3,2,1)
+    %plot(t,d234(24),'rs'); hold on; drawnow
+    %ylabel('[^2^3^4Th_d] (dpm/m^3)')
+    %xlabel('time, (day)')
     
-    subplot(3,2,2)
-    plot(t,d230(24),'rs');hold on; drawnow
-    ylabel('[^2^3^0Th_d (dpm/m^3)')
-    xlabel('time, (day)')
+    %subplot(3,2,2)
+    %plot(t,d230(24),'rs');hold on; drawnow
+    %ylabel('[^2^3^0Th_d (dpm/m^3)')
+    %xlabel('time, (day)')
     
-    subplot(3,2,3)
-    plot(t,TH4(24),'rs');hold on; drawnow
-    ylabel('[^2^3^4Th_f] (dpm/m^3)')
-    xlabel('time, (day)')
+    %subplot(3,2,3)
+    %plot(t,TH4(24),'rs');hold on; drawnow
+    %ylabel('[^2^3^4Th_f] (dpm/m^3)')
+    %xlabel('time, (day)')
     
-    subplot(3,2,4)
-    plot(t,TH0(24),'rs'); hold on;drawnow
-    ylabel('[^2^3^0Th_f] (dpm/m^3)')
-    xlabel('time, (day)')
+    %subplot(3,2,4)
+    %plot(t,TH0(24),'rs'); hold on;drawnow
+    %ylabel('[^2^3^0Th_f] (dpm/m^3)')
+    %xlabel('time, (day)')
     
-    subplot(3,2,5)
-    plot(t,Th4(24),'rs');hold on;
-    ylabel('[^2^3^4Th_s] (dpm/m^3)')
-    xlabel('time, (day)')
+    %subplot(3,2,5)
+    %plot(t,Th4(24),'rs');hold on;
+    %ylabel('[^2^3^4Th_s] (dpm/m^3)')
+    %xlabel('time, (day)')
     
-    subplot(3,2,6)
-    plot(t,Th0(24),'rs');hold on;drawnow
-    ylabel('[^2^3^0Th_s] (dpm/m^3)')
-    xlabel('time, (day)')
+    %subplot(3,2,6)
+    %plot(t,Th0(24),'rs');hold on;drawnow
+    %ylabel('[^2^3^0Th_s] (dpm/m^3)')
+    %xlabel('time, (day)')
     
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    figure(2)
-    subplot(2,3,1)
-    plot(d234,grd.zt,'rs');
-    ylim([0 6000])
-    set(gca,'ydir','reverse','XAxisLocation','top')
-    ylabel('depth (m)')
-    xlabel('[^2^3^4Th_d] (mmol/L)')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %figure(2)
+    %subplot(2,3,1)
+    %plot(d234,grd.zt,'rs');
+    %ylim([0 6000])
+    %set(gca,'ydir','reverse','XAxisLocation','top')
+    %ylabel('depth (m)')
+    %xlabel('[^2^3^4Th_d] (mmol/L)')
     
-    subplot(2,3,4)
-    plot(d230,grd.zt,'rs');
-    ylim([0 6000])
-    set(gca,'ydir','reverse','XAxisLocation','top')
-    ylabel('depth (m)')
-    xlabel('[^2^3^0Th_d] (mmol/L)')
+    %subplot(2,3,4)
+    %plot(d230,grd.zt,'rs');
+    %ylim([0 6000])
+    %set(gca,'ydir','reverse','XAxisLocation','top')
+    %ylabel('depth (m)')
+    %xlabel('[^2^3^0Th_d] (mmol/L)')
     
-    subplot(2,3,2)
-    plot(Th4,grd.zt,'rs');
-    ylim([0 6000])
-    set(gca,'ydir','reverse','XAxisLocation','top')
-				%ylabel('depth (m)')
-    xlabel('[^2^3^4Th_s] (\mumol/L)')
+    %subplot(2,3,2)
+    %plot(Th4,grd.zt,'rs');
+    %ylim([0 6000])
+    %set(gca,'ydir','reverse','XAxisLocation','top')
+				%%ylabel('depth (m)')
+    %xlabel('[^2^3^4Th_s] (\mumol/L)')
     
-    subplot(2,3,5)
-    plot(Th0,grd.zt,'rs');
-    ylim([0 6000])
-    set(gca,'ydir','reverse','XAxisLocation','top')
-				%ylabel('depth (m)')
-    xlabel('[^2^3^0Th_s] (\mumol/L)')
+    %subplot(2,3,5)
+    %plot(Th0,grd.zt,'rs');
+    %ylim([0 6000])
+    %set(gca,'ydir','reverse','XAxisLocation','top')
+				%%ylabel('depth (m)')
+    %xlabel('[^2^3^0Th_s] (\mumol/L)')
     
-    subplot(2,3,3)
-    plot(TH4,grd.zt,'rs');
-    ylim([0 6000])
-    set(gca,'ydir','reverse','XAxisLocation','top')
-				%ylabel('depth (m)')
-    xlabel('[^2^3^4Th_f] (\mumol/L)')
+    %subplot(2,3,3)
+    %plot(TH4,grd.zt,'rs');
+    %ylim([0 6000])
+    %set(gca,'ydir','reverse','XAxisLocation','top')
+				%%ylabel('depth (m)')
+    %xlabel('[^2^3^4Th_f] (\mumol/L)')
     
-    subplot(2,3,6)
-    plot(TH0,grd.zt,'rs');
-    ylim([0 6000])
-    set(gca,'ydir','reverse','XAxisLocation','top')
-    xlabel('[^2^3^0Th_f] (\mumol/L)')
+    %subplot(2,3,6)
+    %plot(TH0,grd.zt,'rs');
+    %ylim([0 6000])
+    %set(gca,'ydir','reverse','XAxisLocation','top')
+    %xlabel('[^2^3^0Th_f] (\mumol/L)')
     
-  end
+  %end
   
 end
 
